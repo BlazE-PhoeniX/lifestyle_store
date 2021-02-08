2efc4f67c38d2cab2f15b0c9f6088fb587a6ac98
BlazE-PhoeniX <hariprasathh01@gmail.com>
2021-02-09 00:01:17 +0530

    updated composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ced072cfe796501ce27741b630b45099c9f0f4b5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ced072cfe796501ce27741b630b45099c9f0f4b5',
+    ),
+  ),
+);
