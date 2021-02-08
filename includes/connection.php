4eb8e8a61659c3240bc05c072f056186c157ed51
BlazE-PhoeniX <hariprasathh01@gmail.com>
2021-02-08 23:25:41 +0530

    configured database

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 //make an connection with the data base
-$con = mysqli_connect("/*your db host*/", "/*your db username*/", "/*your db password*/", "/*your db name*/")
+$con = mysqli_connect("l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "mi49jq8w2ff8lxsm", "amr7hf004gj8n1c0", "cepdnk81om5otwfq")
 or die(mysqli_error($con));
 
 //start the session
